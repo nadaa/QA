6f79d4db0746fdbaf1bb45b9198d700d72ed3275
nadaa <nada.saif7@gmail.com>
2018-11-21 15:54:42 +0200

    change in localhost

--- client/src/services/AddExpenseService.js
+++
@@ -1,9 +0,0 @@
-import Api from '@/services/Api'
-
-export default {
-    saveExpense(data) {
-        Api().post('/addexpense', data)  // data is an object {month,category}
-        .then(response => alert (response.data.msg))
-        // console.log('post expense to db', data)
-    }
-}
